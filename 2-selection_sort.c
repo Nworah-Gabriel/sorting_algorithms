fe1298b27d444f31275026c693a6fbf37ecdf8db
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-08-15 17:45:12 +0100

    Update 2-selection_sort.c
    Fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,6 +4,8 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, tmp;
 
+        if (array == NULL || size < 2)
+            return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = i + 1; j < size; j++)
