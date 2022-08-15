a40f4bf38072edd51953a622a5d3260508ffc6bf
Nworah-Gabriel <gabrielnworah6@gmail.com>
2022-08-14 21:52:08 -0700

    My task commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a fuction that sorts an array by comparison in every iteration
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return - None
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int j, tmp, x, k;
+
+	for (x = 0; x < size ; size--)
+	{
+		for (j = 0; j < size - x; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				tmp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = tmp;
+			}
+		}
+		k = 0;
+		for ( ; k < size; k++)
+		{
+			if (k > 0)
+				printf(", ");
+			printf("%d", array[k]);
+		}
+		printf("\n");
+	}
+}
