aaa3c74ae5ace53368087f754da0a3896f283a4a
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-08-15 17:00:30 +0100

    Update and rename selection_sort.c to 2-selection_sort.c
    Fixed

---
+++ 2-selection_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+			else
+				continue;
+
+		}
+	}
+}
